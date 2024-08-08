ce2d78e3b69b3da43c0681b3f52b0b4458e47c4d
perhapsmaybe <68890108+perhapsmaybe@users.noreply.github.com>
2024-08-07 19:35:10 -0700

    Update punishment.ts

--- scripts/commands/punishment.ts
+++ scripts/commands/punishment.ts
@@ -2,10 +2,10 @@ import { ChatSendBeforeEvent } from "@minecraft/server";
 
 export default {
     name: "punishment",
+    requiredRank: 1,
     callback: (event: ChatSendBeforeEvent) => {
         const player = event.sender;
         const message = event.message;
-
-
+        const args = message.split(" ").shift();
     }
 }
