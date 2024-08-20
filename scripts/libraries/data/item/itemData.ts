a1414d8b3a1f7ab022294f55355223ede873c25d
perhapsmaybe <118870356+perhaps0309Lua@users.noreply.github.com>
2024-08-19 17:13:04 -0700

    ItemData syntax
    Prepare ItemData to be moved into a seperate project with documentation. Remove old ItemData

--- scripts/libraries/data/item/itemData.ts
+++ scripts/libraries/data/item/itemData.ts
@@ -20,11 +20,30 @@ export class ItemData {
         this.slot = slot; // You need to set the slot for anything besides lore adjustments.
     }
 
+    /**
+     * Get a dynamic property from a non-stackable ItemStack
+     * @param {string} key - The key of the dynamic property
+     * @example
+     * const playerStats = itemData.getDynamicProperty("playerStats");
+     * if (playerStats) {
+     *   console.log(playerStats.health);
+     *   console.log(playerStats.attack);
+     * }
+     * @returns {any}
+     */
     public getDynamicProperty(key: string): any {
         let value = this.item.getDynamicProperty(key);
         return safeJsonParser(value);
     }
 
+    /**
+     * Set a dynamic property on a non-stackable ItemStack
+     * @param {string} key - The key of the dynamic property
+     * @param {any} value - The value of the dynamic property
+     * @example
+     * itemData.setDynamicProperty("playerStats", {health: 20, attack: 10});
+     * @returns {void}
+     */
     public setDynamicProperty(key: string, value: any): void {
         if (value === undefined) throw Error("Invalid value.");
         if (typeof value === "object") value = safeJsonStringify(value);
@@ -34,21 +53,52 @@ export class ItemData {
     }
 
     // Lore functions
+    /**
+     * Get the lore of the item, returns an array of strings
+     * @example
+     * const lore = itemData.getLore();
+     * console.log(lore);
+     * // ["This is a line of lore", "This is another line of lore"]
+     * @returns {string[]}
+     */
     public getLore(): string[] {
         return this.item.getLore();
     }
 
+    /**
+     * Set the lore of the item
+     * @param {string[]} lore - The lore to set
+     * @example
+     * itemData.setLore(["This is a line of lore", "This is another line of lore"]);
+     */
     public setLore(lore: string[]): void {
         this.item.setLore(lore);
         this.updateItem();
     }
 
+    /**
+     * Add a line of lore to the item
+     * @param {string} lore - The lore to add
+     * @example 
+     * itemData.addLore("This is a line of lore");
+     */
     public addLore(lore: string): void {
         const currentLore = this.getLore();
         currentLore.push(lore);
         this.setLore(currentLore);
     }
 
+    /**
+     * Remove a line of lore from the item
+     * @param {string | number} lore - The lore to remove
+     * @example 
+     * // Remove the first line of lore
+     * itemData.removeLore(0);
+     * 
+     * // Remove a specific line of lore
+     * itemData.removeLore("This is a line of lore");
+     * @returns {void}
+     */
     public removeLore(lore: string | number): void {
         const currentLore = this.getLore();
         if (typeof lore === "number") {
@@ -63,6 +113,32 @@ export class ItemData {
         this.setLore(currentLore);
     }
 
+    // Custom Lore functions
+
+    /**
+     * Get the custom lore of the item, returns an object with string or string[] values
+     * @example
+     * const customLore = itemData.getCustomLore();
+     * console.log(customLore);
+     * // { "Category": ["This is a line of lore", "This is another line of lore"] }
+     * @returns {{ [key: string]: string | string[] }}
+     */
+    public getCustomLore(): { [key: string]: string | string[] } {
+        return this.getDynamicProperty("lore") || {};
+    }
+
+    /**
+     * Adds custom lore to the item, centered in the middle of the lore with category titles
+     * @param {{ [key: string]: string | string[] }} customLore - The custom lore to set
+     * @param {string} category - The category of the custom lore
+     * @example
+     * itemData.addCustomLore("This is a line of custom lore", "LoreCategory");
+     * itemData.addCustomLore(["This is a line of custom lore", "This is another line of custom lore"], "LoreCategory");
+     * itemData.addCustomLore("This is a line of custom lore"); // Doesn't require categories
+     * 
+     * // Update the lore
+     * itemData.updateLore();
+     */
     public addCustomLore(lore: string | string[], category?: string): void {
         let customLore = this.getDynamicProperty("lore") || {};
         const index = typeof lore === "string" ? lore : lore[0];
@@ -70,6 +146,18 @@ export class ItemData {
         this.setDynamicProperty("lore", customLore);
     }
 
+    /**
+     * Removes custom lore from the item
+     * @param {string | string[]} lore - The custom lore to remove
+     * @param {string} category - The category of the custom lore to remove
+     * @example
+     * itemData.removeCustomLore("This is a line of custom lore", "LoreCategory"); // Remove lore inside a category
+     * itemData.removeCustomLore("LoreCategory") // Remove the entire category
+     * itemData.removeCustomLore("This is a line of custom lore"); // Remove lore without a category
+     * 
+     * // Update the item to update the lore
+     * itemData.updateLore();
+     */
     public removeCustomLore(lore: string | string[], category?: string): void {
         let customLore = this.getDynamicProperty("lore") || {};
         const index = typeof lore === "string" ? lore : lore[0];
@@ -78,15 +166,36 @@ export class ItemData {
     }
 
     // Enchantment functions
+    /**
+     * Get the enchantments of the item, returns an object with EnchantmentDataT values
+     * @example
+     * const enchantments = itemData.getEnchantments();
+     * console.log(enchantments); // { "sharpness": { name: "sharpness", level: 1 } }
+     * @returns {{ [key: string]: EnchantmentDataT }}
+     */
     public getEnchantments(): { [key: string]: EnchantmentDataT } {
         return this.getDynamicProperty("enchantments") || {};
     }
 
+    /**
+     * Set the enchantments of the item
+     * @param {{ [key: string]: EnchantmentDataT }} enchantments - The enchantments to set
+     * @example
+     * itemData.setEnchantments({ "sharpness": { name: "sharpness", level: 1 } });
+     * @returns {void}
+     */
     public setEnchantments(enchantments: { [key: string]: EnchantmentDataT }): void {
         this.setDynamicProperty("enchantments", enchantments);
         this.updateLore();
     }
 
+    /**
+     * Add an enchantment to the item
+     * @param {EnchantmentDataT} enchantment - The enchantment to add
+     * @example
+     * itemData.addEnchantment({ name: "sharpness", level: 1 });
+     * @returns {void}
+     */
     public addEnchantment(enchantment: EnchantmentDataT): void {
         const enchantments = this.getEnchantments();
         enchantments[enchantment.name] = enchantment;
@@ -94,6 +203,13 @@ export class ItemData {
         this.setEnchantments(enchantments);
     }
 
+    /**
+     * Remove an enchantment from the item
+     * @param {string} enchantment - The enchantment to remove
+     * @example
+     * itemData.removeEnchantment("sharpness");
+     * @returns {void}
+     */
     public removeEnchantment(enchantment: string): void {
         const enchantments = this.getEnchantments();
         delete enchantments[enchantment];
@@ -102,14 +218,35 @@ export class ItemData {
     }
 
     // Effect functions 
+    /**
+     * Get the effects of the item, returns an object with EffectDataT values
+     * @example
+     * const effects = itemData.getEffects();
+     * console.log(effects); // { "speed": { name: "speed", level: 1 } }
+     * @returns {{ [key: string]: any }}
+     */
     public getEffects(): { [key: string]: any } {
         return this.getDynamicProperty("effects") || {};
     }
 
+    /**
+     * Set the effects of the item
+     * @param {{ [key: string]: any }} effects - The effects to set
+     * @example
+     * itemData.setEffects({ "speed": { name: "speed", level: 1 } });
+     * @returns {void}
+     */
     public setEffects(effects: { [key: string]: any }): void {
         this.setDynamicProperty("effects", effects);
     }
 
+    /**
+     * Add an effect to the item, you still have to manually setup the item effects (examples in GitHub)
+     * @param {ItemEffectDataT} effect - The effect to add
+     * @example
+     * itemData.addEffect({ name: "speed", level: 1 });
+     * @returns {void}
+     */
     public addEffect(effect: ItemEffectDataT): void {
         const effects = this.getEffects();
         effects[effect.effect] = effect;
@@ -117,6 +254,13 @@ export class ItemData {
         this.setEffects(effects);
     }
 
+    /**
+     * Remove an effect from the item
+     * @param {string} effect - The effect to remove
+     * @example
+     * itemData.removeEffect("speed");
+     * @returns {void}
+     */
     public removeEffect(effect: string): void {
         const effects = this.getEffects();
         delete effects[effect];
@@ -125,6 +269,14 @@ export class ItemData {
     }
 
     // Dynamic property functions
+    /**
+     * Update the item, this is required after setting dynamic properties
+     * @returns 
+     * @example 
+     * this.addCustomLore("This is a line of custom lore", "LoreCategory");
+     * this.updateLore(); 
+     * // When you get the item, it returns a copy of the item, so you need to update the item in the player's inventory
+     */
     public updateItem(): void {
         if (this.slot === undefined) return;
         if (typeof this.slot === "number") {
@@ -142,6 +294,15 @@ export class ItemData {
         }
     }
 
+    /**
+     * Update the lore of the item, this is required after setting custom lore, enchantments, or effects
+     * @returns 
+     * @example 
+     * this.addCustomLore("This is a line of custom lore", "LoreCategory");
+     * this.updateLore(); 
+     * // When you get the item, it returns a copy of the item, so you need to update the item in the player's inventory
+     * @returns {void}
+     */
     public updateLore(): void {
         const longestText = "Has Custom Properties";
         let enchantments = this.getEnchantments();
--- scripts/libraries/data/player/playerData.ts
+++ scripts/libraries/data/player/playerData.ts
@@ -1,63 +1,6 @@
 import { world, Player, Vector3, EntityComponentTypes, EntityEquippableComponent, EquipmentSlot } from "@minecraft/server";
 import { EffectDataT } from "../../../types";
 import { rankPriority } from "./ranks";
-export const PlayerDataHandler = {
-    // Gets the value of a property from the player
-    // If it's JSON, then it will automatically be parsed
-    get(key: string, player: Player) {
-        let value = player.getDynamicProperty(key);
-        try { value = JSON.parse(value as string); } catch (err) { }
-        return value;
-    },
-
-    // Sets the value of a property on the player
-    set(key: string, value: any, player: Player) {
-        if (value === undefined) throw Error("Invalid value.");
-        if (typeof value === "object") value = JSON.stringify(value);
-        player.setDynamicProperty(key, value);
-    },
-
-    // Checks if the player has a property
-    has(key: string, player: Player) {
-        return !!(player).getDynamicProperty(key);
-    },
-
-    // Deletes a property from the player
-    delete(key: string, player: Player) {
-        player.setDynamicProperty(key, undefined);
-    },
-
-    // Gets all of a player's data
-    entries(player: Player) {
-        const dynamicPropertyIds = player.getDynamicPropertyIds();
-        const entries: { [key: string]: any } = {}
-        for (const dynamicPropertyKey of dynamicPropertyIds) {
-            entries[dynamicPropertyKey] = this.get(dynamicPropertyKey, player);
-        }
-
-        return entries;
-    },
-
-    // Deletes all of a player's data
-    deleteAll(player: Player) {
-        const playerData = this.entries(player);
-        for (const dynamicPropertyKey of Object.keys(playerData)) {
-            this.delete(dynamicPropertyKey, player);
-        }
-    },
-
-    // Loops through all players and get their data
-    getAll() {
-        const players = world.getPlayers();
-        const data: { [key: string]: any } = {};
-        for (const player of players) {
-            data[player.name] = this.entries(player); // Get current data for the player
-            data[player.name].player = player; // Append the player object onto the player's data
-        }
-
-        return data;
-    }
-};
 
 // Gets a player's object from their name.
 // Handles player's with spaces in their names aswell.
